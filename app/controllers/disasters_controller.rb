81e19402b776be8cf36c2ec7fe56a5c7e1b47573
ArtOfCode- <hello@artofcode.co.uk>
2017-09-08 23:09:51 +0100

    License and start disasters controller

--- app/controllers/disasters_controller.rb
+++ app/controllers/disasters_controller.rb
@@ -1,8 +1,29 @@
 class DisastersController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_admin
+  before_action :require_admin, except: [:index]
 
   def index
     @disasters = Disaster.active.paginate(page: params[:page], per_page: 25)
+  end
+
+  def new
+    @disaster = Disaster.new
+  end
+
+  def create
+    @disaster = Disaster.create disaster_params
+    if @disaster.save
+      flash[:success] = "Success - disaster response functionality is now available for #{@disaster.name}."
+      redirect_to disasters_path
+    else
+      flash[:danger] = 'Failed to save record - please try again and contact support if the issue persists.'
+      render :new
+    end
+  end
+
+  private
+
+  def disaster_params
+    params.require(:disaster).permit(:name, :description, :active)
   end
 end
