37cc287f2d5796da5c41f3dd4f92c381032a99ad
Isaiah Zwick-Schachter <isaiah@developingtechnician.com>
2017-09-10 10:33:17 -0400

    Aded new form

--- app/controllers/rescue_requests_controller.rb
+++ app/controllers/rescue_requests_controller.rb
@@ -1,2 +1,17 @@
 class RescueRequestsController < ApplicationController
+  def new
+  end
+
+  def create
+    render plain: Disaster.find(params[:disaster_id]).rescue_requests.create(lat: params[:lat], long: params[:long]).id
+  end
+
+  def update_short
+    RescueRequest.find(params[:person_id]).update(params.permit(RescueRequest.column_names).to_h)
+  end
+
+  def update_long
+    RescueRequest.find(params[:person_id]).update(params.permit(RescueRequest.column_names).to_h)
+    redirect_to root_path
+  end
 end
