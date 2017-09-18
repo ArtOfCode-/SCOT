344383a637848541a81653bd75d951188565d4ac
izwick-schachter <isaiah@developingtechnician.com>
2017-09-18 13:57:30 -0400

    Simplified deduping flow and hid duplicates from the disaster index

--- app/controllers/queues_controller.rb
+++ app/controllers/queues_controller.rb
@@ -9,19 +9,25 @@ class QueuesController < ApplicationController
     possible_duplicate_columns = %w[twitter phone email medical conditions extra_details street_address]
     filtered_request = @original.attributes.select { |col, _val| possible_duplicate_columns.include? col }
     conditions = filtered_request.map { |col, val| " #{col} LIKE #{ActiveRecord::Base.connection.quote("%#{val.to_s}%")}" unless val.to_s.empty? }.reject { |i| i.to_s.empty? }.join(" OR ")
-    @suggested_dupes = RescueRequest.where(conditions)
+    @suggested_dupes = RescueRequest.where(conditions).where.not(id: @original.id)
   end
 
   # Dupe_of 0 means that it's a new record
   def dedupe_complete
+    original = RescueRequest.find(params[:rescue_request_id])
     if params[:dupe_of]
-      current_user.dedupe_reviews.create(rescue_request_id: params[:original_id], outcome: "dupe", dupe_of_id: params[:dupe_of], suggested_duplicates: params[:suggested_duplicates])
-      RescueRequest.find(params[:rescue_request_id]).update(dupe_of: params[:dupe_of])
+      target = RescueRequest.find(params[:dupe_of])
+      until target.dupe_of.nil? || target.dupe_of == 0
+        target = RescueRequest.find(target.dupe_of)
+      end
+      target.update(dupe_of: 0) if target.dupe_of == nil
+      current_user.dedupe_reviews.create(rescue_request_id: original.id, outcome: "dupe", dupe_of_id: target.id, suggested_duplicates: params[:suggested_duplicates])
+      original.update(dupe_of: params[:dupe_of])
     elsif params[:skip]
-      current_user.dedupe_reviews.create(rescue_request_id: params[:original_id], outcome: "skip", suggested_duplicates: params[:suggested_duplicates])
+      current_user.dedupe_reviews.create(rescue_request_id: original.id, outcome: "skip", suggested_duplicates: params[:suggested_duplicates])
     elsif params[:not]
-      current_user.dedupe_reviews.create(rescue_request_id: params[:original_id], outcome: "not", dupe_of_id: 0, suggested_duplicates: params[:suggested_duplicates])
-      RescueRequest.find(params[:rescue_request_id]).update(dupe_of: 0)
+      current_user.dedupe_reviews.create(rescue_request_id: original.id, outcome: "not", dupe_of_id: 0, suggested_duplicates: params[:suggested_duplicates])
+      original.update(dupe_of: 0)
     end
     redirect_to action: :dedupe
   end
