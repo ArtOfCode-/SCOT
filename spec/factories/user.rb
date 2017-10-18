d82d6df8975eecb388ddfc3ddca40e58a0ef2ca7
Nicholas Sellek <nick@seansellek.com>
2017-10-17 20:38:53 -0400

    Added rest of role factories and added role traits to user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,6 +5,30 @@ FactoryGirl.define do
     password 'test_pass'
     password_confirmation 'test_pass'
     username 'tester'
+
+    trait :developer do
+      association :roles, factory: :dev_role
+    end
+
+    trait :triage do
+      association :roles, factory: :triage_role
+    end
+
+    trait :rescue do
+      association :roles, factory: :rescue_role
+    end
+
+    trait :medical do
+      association :roles, factory: :medical_role
+    end
+
+    trait :broadcaster do
+      association :roles, factory: :broadcast_role
+    end
+
+    trait :miner do
+      association :roles, factory: :miner_role
+    end
   end
 
   factory :admin, class: User do
