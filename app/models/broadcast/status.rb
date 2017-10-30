90543b737a088cf933c60abf548dcead5951a6a2
ArtOfCode- <hello@artofcode.co.uk>
2017-10-30 15:14:09 +0000

    Some unique constraints

--- app/models/broadcast/status.rb
+++ app/models/broadcast/status.rb
@@ -1,6 +1,8 @@
 class Broadcast::Status < ApplicationRecord
   has_many :items, class_name: 'Broadcast::Item', foreign_key: 'status_id'
 
+  validates :name, uniqueness: true
+
   def self.[](key)
     find_by name: key
   end
--- app/models/medical_condition.rb
+++ app/models/medical_condition.rb
@@ -1,3 +1,5 @@
 class MedicalCondition < ApplicationRecord
   has_and_belongs_to_many :rescue_requests
+
+  validates :name, uniqueness: true
 end
