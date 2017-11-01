b676a3fc389d7f511dc31b0a95d8955fd7d7beb8
ArtOfCode- <hello@artofcode.co.uk>
2017-11-01 21:23:20 +0000

    API helpers

--- app/api/base.rb
+++ app/api/base.rb
@@ -1,7 +1,33 @@
 module API
   class Base < Grape::API
+    version 'v1', using: :path
+    prefix :api
+    format :json
+
     helpers do
-      # TODO current_user and authentication/authorization helpers
+      def current_user
+        @current_user ||= @token.user
+      end
+
+      def authenticate_app!
+        @key = APIKey.find_by key: params[:key]
+        error!({ name: 'missing_key', detail: 'No key was provided or the provided key is invalid.' }, 403) unless @key.present?
+      end
+
+      def authenticate_user!
+        @token = @key.api_tokens.find_by token: params[:token]
+        error!({ name: 'missing_token', detail: 'No token was provided or the provided token is invalid.' }, 401) unless @token.present?
+      end
+
+      def authenticate_full!
+        authenticate_app!
+        authenticate_user!
+      end
+
+      def role(*names)
+        return if current_user.has_any_role?(*names)
+        error!({ name: 'unauthorized', detail: 'The authenticated user does not have the required permissions for this action.' }, 403)
+      end
     end
   end
 end
