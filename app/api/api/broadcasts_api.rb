98e829f4d0e8410b4f2b2f16ccc292c6f863ec01
ArtOfCode- <hello@artofcode.co.uk>
2017-11-13 23:25:23 +0000

    More API

--- app/api/api/broadcasts_api.rb
+++ app/api/api/broadcasts_api.rb
@@ -21,6 +21,64 @@ module API
       get '/review' do
         std_result Broadcast::Item.active.where(status: Broadcast::Status['Pending Review'])
       end
+
+      params do
+        requires :key, type: String
+        requires :token, type: String
+
+        optional :content, type: String
+        optional :translation, type: String
+        optional :originated_at, type: DateTime
+        optional :broadcast_municipality_id, type: Integer
+        optional :source, type: String
+        optional :top, type: Boolean
+        optional :bottom, type: Boolean
+        optional :notes, type: String
+      end
+      post '/new' do
+        authenticate_user! && role(:broadcast, :miner)
+        item_params = params.except(:key, :token, :content, :translation)
+
+        @item = Broadcast::Item.new item_params.merge(user: current_user)
+        if !params[:content].nil? || !params[:translation].nil?
+          now = DateTime.now.utc
+          if now.hour >= 0 && now.hour < 14
+            now = now.change(hour: 14)
+          elsif now.hour >= 14
+            now = (now + 1.day).change(hour: 0)
+          end
+          if !params[:translation].present?
+            from = Translations::Language['en-US']
+            to = Translations::Language['es-PR']
+            content = params[:content]
+          elsif !params[:content].present?
+            from = Translations::Language['es-PR']
+            to = Translations::Language['en-US']
+            content = params[:translation]
+          else
+            from = Translations::Language['en-US']
+            to = Translations::Language['es-PR']
+            content = params[:content]
+            final = params[:translation]
+            status = Translations::Status['Completed']
+          end
+
+          success = ApplicationRecord.status_transaction do
+            @item.save
+            @item.translations.create(content: content, source_lang: from, target_lang: to, deliver_to: 'SCOT', due: now,
+                                      requester: current_user, priority: Translations::Priority['Next Broadcast'],
+                                      final: final, status: status)
+          end
+
+          if success
+            std_result @item
+          else
+            error!({name: 'save_failed', detail: 'Creation transaction failed'}, 500)
+          end
+        else
+          error!({name: 'missing_data', detail: 'Either content or translation is required'}, 400)
+        end
+      end
     end
 
     resource :municipalities do
