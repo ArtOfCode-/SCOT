6caa18d3e74dc70c05dd78a82555c909654e4b9b
Isaiah Zwick-Schachter <isaiah@developingtechnician.com>
2017-11-25 22:28:53 -0500

    Validations for required attributes

--- app/models/rescue_request.rb
+++ app/models/rescue_request.rb
@@ -20,4 +20,8 @@ class RescueRequest < ApplicationRecord
   has_many :resource_uses, class_name: 'Dispatch::ResourceUse', foreign_key: 'request_id'
   has_many :resources, through: :resource_uses, class_name: 'Dispatch::Resource'
   belongs_to :rescue_crew, class_name: 'Dispatch::RescueCrew', optional: true
+
+  validates :lat, presence: true
+  validates :long, presence: true
+  validates :name, presence: true
 end
